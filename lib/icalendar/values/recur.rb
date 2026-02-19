cd44bd3b5e30d3ab5a430c8a36d7b09a50ae73d7
Bill Rastello <brastello@skylightframe.com>
2026-02-19 09:53:28 -0600

    Replace 14 regex scans in Recur#parse_fields with split-based hash lookup

--- lib/icalendar/values/recur.rb
+++ lib/icalendar/values/recur.rb
@@ -10,12 +10,6 @@ module Icalendar
                                :by_month, :by_set_position, :week_start,
                                keyword_init: true)
 
-      NUM_LIST = '\d{1,2}(?:,\d{1,2})*'
-      DAYNAME = 'SU|MO|TU|WE|TH|FR|SA'
-      WEEKDAY = "(?:[+-]?\\d{1,2})?(?:#{DAYNAME})"
-      MONTHDAY = '[+-]?\d{1,2}'
-      YEARDAY = '[+-]?\d{1,3}'
-
       def initialize(value, *args)
         if value.is_a? Icalendar::Values::Recur
           super value.value, *args
@@ -50,37 +44,28 @@ module Icalendar
 
       private
 
-      PARSE_FIELDS_FREQUENCY_REGEX = /FREQ=(SECONDLY|MINUTELY|HOURLY|DAILY|WEEKLY|MONTHLY|YEARLY)/i.freeze
-      PARSE_FIELDS_UNTIL_REGEX = /UNTIL=([^;]*)/i.freeze
-      PARSE_FIELDS_COUNT_REGEX = /COUNT=(\d+)/i.freeze
-      PARSE_FIELDS_INTERVAL_REGEX = /INTERVAL=(\d+)/i.freeze
-      PARSE_FIELDS_BY_SECOND_REGEX = /BYSECOND=(#{NUM_LIST})(?:;|\z)/i.freeze
-      PARSE_FIELDS_BY_MINUTE_REGEX = /BYMINUTE=(#{NUM_LIST})(?:;|\z)/i.freeze
-      PARSE_FIELDS_BY_HOUR_REGEX = /BYHOUR=(#{NUM_LIST})(?:;|\z)/i.freeze
-      PARSE_FIELDS_BY_DAY_REGEX = /BYDAY=(#{WEEKDAY}(?:,#{WEEKDAY})*)(?:;|\z)/i.freeze
-      PARSE_FIELDS_BY_MONTH_DAY_REGEX = /BYMONTHDAY=(#{MONTHDAY}(?:,#{MONTHDAY})*)(?:;|\z)/i.freeze
-      PARSE_FIELDS_BY_YEAR_DAY_REGEX = /BYYEARDAY=(#{YEARDAY}(?:,#{YEARDAY})*)(?:;|\z)/i.freeze
-      PARSE_FIELDS_BY_WEEK_NUMBER_REGEX = /BYWEEKNO=(#{MONTHDAY}(?:,#{MONTHDAY})*)(?:;|\z)/i.freeze
-      PARSE_FIELDS_BY_MONTH_REGEX = /BYMONTH=(#{NUM_LIST})(?:;|\z)/i.freeze
-      PARSE_FIELDS_BY_SET_POSITON_REGEX = /BYSETPOS=(#{YEARDAY}(?:,#{YEARDAY})*)(?:;|\z)/i.freeze
-      PARSE_FIELDS_BY_WEEK_START_REGEX = /WKST=(#{DAYNAME})/i.freeze
-
       def parse_fields(value)
+        parts = {}
+        value.split(';').each do |segment|
+          key, val = segment.split('=', 2)
+          parts[key.upcase] = val if key && val
+        end
+
         {
-          frequency: (value =~ PARSE_FIELDS_FREQUENCY_REGEX ? $1.upcase : nil),
-          until: (value =~ PARSE_FIELDS_UNTIL_REGEX ? $1 : nil),
-          count: (value =~ PARSE_FIELDS_COUNT_REGEX ? $1.to_i : nil),
-          interval: (value =~ PARSE_FIELDS_INTERVAL_REGEX ? $1.to_i : nil),
-          by_second: (value =~ PARSE_FIELDS_BY_SECOND_REGEX ? $1.split(',').map { |i| i.to_i } : nil),
-          by_minute: (value =~ PARSE_FIELDS_BY_MINUTE_REGEX ? $1.split(',').map { |i| i.to_i } : nil),
-          by_hour: (value =~ PARSE_FIELDS_BY_HOUR_REGEX ? $1.split(',').map { |i| i.to_i } : nil),
-          by_day: (value =~ PARSE_FIELDS_BY_DAY_REGEX ? $1.split(',') : nil),
-          by_month_day: (value =~ PARSE_FIELDS_BY_MONTH_DAY_REGEX ? $1.split(',') : nil),
-          by_year_day: (value =~ PARSE_FIELDS_BY_YEAR_DAY_REGEX ? $1.split(',') : nil),
-          by_week_number: (value =~ PARSE_FIELDS_BY_WEEK_NUMBER_REGEX ? $1.split(',') : nil),
-          by_month: (value =~ PARSE_FIELDS_BY_MONTH_REGEX ? $1.split(',').map { |i| i.to_i } : nil),
-          by_set_position: (value =~ PARSE_FIELDS_BY_SET_POSITON_REGEX ? $1.split(',') : nil),
-          week_start: (value =~ PARSE_FIELDS_BY_WEEK_START_REGEX ? $1.upcase : nil)
+          frequency:       parts['FREQ']&.upcase,
+          until:           parts['UNTIL'],
+          count:           parts['COUNT']&.to_i,
+          interval:        parts['INTERVAL']&.to_i,
+          by_second:       parts['BYSECOND']&.split(',')&.map(&:to_i),
+          by_minute:       parts['BYMINUTE']&.split(',')&.map(&:to_i),
+          by_hour:         parts['BYHOUR']&.split(',')&.map(&:to_i),
+          by_day:          parts['BYDAY']&.split(','),
+          by_month_day:    parts['BYMONTHDAY']&.split(','),
+          by_year_day:     parts['BYYEARDAY']&.split(','),
+          by_week_number:  parts['BYWEEKNO']&.split(','),
+          by_month:        parts['BYMONTH']&.split(',')&.map(&:to_i),
+          by_set_position: parts['BYSETPOS']&.split(','),
+          week_start:      parts['WKST']&.upcase
         }
       end
     end
